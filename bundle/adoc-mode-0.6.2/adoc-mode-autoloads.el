36b45b3f944dd8d53ad446f5fc5197d109b65d41
Daniel Grigg <mail@danielgrigg.com>
2013-08-07 20:47:16 +1000

    Add adoc-mode-0.6.2

---
+++ bundle/adoc-mode-0.6.2/adoc-mode-autoloads.el
@@ -0,0 +1,28 @@
+;;; adoc-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads (adoc-mode) "adoc-mode" "adoc-mode.el" (20990 14200
+;;;;;;  0 0))
+;;; Generated autoloads from adoc-mode.el
+
+(autoload 'adoc-mode "adoc-mode" "\
+Major mode for editing AsciiDoc text files.
+Turning on Adoc mode runs the normal hook `adoc-mode-hook'.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("adoc-mode-pkg.el") (20990 14200 597645
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; adoc-mode-autoloads.el ends here
