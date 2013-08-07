5affd6313e802bd0e540ab9b2d7ae7afaeb4a86e
Daniel Grigg <mail@danielgrigg.com>
2013-08-07 20:55:46 +1000

    Add c-eldoc-0.5

---
+++ bundle/c-eldoc-0.5/c-eldoc-autoloads.el
@@ -0,0 +1,16 @@
+;;; c-eldoc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("c-eldoc-pkg.el" "c-eldoc.el") (20990
+;;;;;;  14190 685546 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; c-eldoc-autoloads.el ends here
