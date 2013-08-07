6f123f513cec2306266925ddb7b78eaf0daf564d
Daniel Grigg <mail@danielgrigg.com>
2013-08-07 20:56:43 +1000

    Add project-mode-1.0

---
+++ bundle/project-mode-1.0/project-mode-autoloads.el
@@ -0,0 +1,16 @@
+;;; project-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("project-mode-pkg.el" "project-mode.el")
+;;;;;;  (20990 14154 643134 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; project-mode-autoloads.el ends here
