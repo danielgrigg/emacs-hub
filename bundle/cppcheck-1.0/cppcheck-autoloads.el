8125f7fb39f53963328b9f811bab042e7363599f
Daniel Grigg <mail@danielgrigg.com>
2013-08-07 20:46:17 +1000

    Add cppcheck-1.0

---
+++ bundle/cppcheck-1.0/cppcheck-autoloads.el
@@ -0,0 +1,27 @@
+;;; cppcheck-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads (cppcheck) "cppcheck" "cppcheck.el" (20990 14151
+;;;;;;  0 0))
+;;; Generated autoloads from cppcheck.el
+
+(autoload 'cppcheck "cppcheck" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("cppcheck-pkg.el") (20990 14151 198762
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; cppcheck-autoloads.el ends here
