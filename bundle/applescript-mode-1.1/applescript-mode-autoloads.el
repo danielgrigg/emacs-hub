34379680da95e937fb6b9c721bed89588350c450
Daniel Grigg <mail@danielgrigg.com>
2013-08-07 20:47:16 +1000

    Add applescript-mode-1.1

---
+++ bundle/applescript-mode-1.1/applescript-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; applescript-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads (applescript-mode) "applescript-mode" "applescript-mode.el"
+;;;;;;  (20990 14195 0 0))
+;;; Generated autoloads from applescript-mode.el
+
+(autoload 'applescript-mode "applescript-mode" "\
+Major mode for editing AppleScript files.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.applescript$" . applescript-mode))
+
+;;;***
+
+;;;### (autoloads nil nil ("applescript-mode-pkg.el") (20990 14195
+;;;;;;  665864 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; applescript-mode-autoloads.el ends here
