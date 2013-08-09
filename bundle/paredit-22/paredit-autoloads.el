84c4b4c7826db9a7923f3a8829e481297abd9fa1
Daniel Casimir Grigg <daniel.grigg@covata.com>
2013-08-09 12:06:04 +1000

    Adding plugin paredit-22

---
+++ bundle/paredit-22/paredit-autoloads.el
@@ -0,0 +1,33 @@
+;;; paredit-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads (paredit-mode) "paredit" "paredit.el" (20996 17601
+;;;;;;  0 0))
+;;; Generated autoloads from paredit.el
+
+(autoload 'paredit-mode "paredit" "\
+Minor mode for pseudo-structurally editing Lisp code.
+With a prefix argument, enable Paredit Mode even if there are
+  imbalanced parentheses in the buffer.
+Paredit behaves badly if parentheses are imbalanced, so exercise
+  caution when forcing Paredit Mode to be enabled, and consider
+  fixing imbalanced parentheses instead.
+\\<paredit-mode-map>
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("paredit-pkg.el") (20996 17601 487642
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; paredit-autoloads.el ends here
