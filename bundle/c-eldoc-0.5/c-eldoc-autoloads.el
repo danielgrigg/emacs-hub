587a011b7a015e9c9dc7f3e595c691e6c0ffe9bd
Daniel Grigg <mail@danielgrigg.com>
2013-08-10 09:35:17 +1000

    Resolve merge

--- bundle/c-eldoc-0.5/c-eldoc-autoloads.el
+++ bundle/c-eldoc-0.5/c-eldoc-autoloads.el
@@ -3,13 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-<<<<<<< HEAD
-;;;### (autoloads nil nil ("c-eldoc-pkg.el" "c-eldoc.el") (20990
-;;;;;;  14190 685546 0))
-=======
 ;;;### (autoloads nil nil ("c-eldoc-pkg.el" "c-eldoc.el") (20996
 ;;;;;;  18015 762472 0))
->>>>>>> 36f75b96f6b4a3fa275f7e73058ab8dd5ad6f727
 
 ;;;***
 
