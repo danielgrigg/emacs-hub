04ac4eae83f852fa84e79a94a4486acda838307b
Daniel Casimir Grigg <daniel.grigg@covata.com>
2013-09-06 11:24:04 +1000

    Add ruby-end-0.2.0

---
+++ bundle/ruby-end-0.2.0/ruby-end-autoloads.el
@@ -0,0 +1,27 @@
+;;; ruby-end-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads (ruby-end-mode) "ruby-end" "ruby-end.el" (21033
+;;;;;;  11883 0 0))
+;;; Generated autoloads from ruby-end.el
+
+(autoload 'ruby-end-mode "ruby-end" "\
+Automatic insertion of end blocks for Ruby.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ruby-end-pkg.el") (21033 11883 699056
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ruby-end-autoloads.el ends here
