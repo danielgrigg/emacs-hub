943108c2b823746fe40a2f47254604f06ae4531d
Daniel Casimir Grigg <daniel.grigg@covata.com>
2013-09-06 14:40:11 +1000

    Adding plugin pyflakes-1.0

---
+++ bundle/pyflakes-1.0/pyflakes-autoloads.el
@@ -0,0 +1,27 @@
+;;; pyflakes-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads (pyflakes) "pyflakes" "pyflakes.el" (21033 23596
+;;;;;;  0 0))
+;;; Generated autoloads from pyflakes.el
+
+(autoload 'pyflakes "pyflakes" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("pyflakes-pkg.el") (21033 23596 109997
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pyflakes-autoloads.el ends here
