3c4f986068e646a9cbe8fca4cab8ca39711598f8
Daniel Casimir Grigg <daniel.grigg@covata.com>
2013-09-06 14:40:11 +1000

    Adding plugin py-import-check-0.2

---
+++ bundle/py-import-check-0.2/py-import-check-autoloads.el
@@ -0,0 +1,27 @@
+;;; py-import-check-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads (py-import-check) "py-import-check" "py-import-check.el"
+;;;;;;  (21033 23596 0 0))
+;;; Generated autoloads from py-import-check.el
+
+(autoload 'py-import-check "py-import-check" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("py-import-check-pkg.el") (21033 23597
+;;;;;;  15195 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; py-import-check-autoloads.el ends here
