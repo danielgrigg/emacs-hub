f847862f87154928b87542815205ff2eeda51489
Daniel Casimir Grigg <daniel.grigg@covata.com>
2013-09-06 11:23:53 +1000

    Add ruby-block-0.0.11

---
+++ bundle/ruby-block-0.0.11/ruby-block-autoloads.el
@@ -0,0 +1,16 @@
+;;; ruby-block-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("ruby-block-pkg.el" "ruby-block.el") (21033
+;;;;;;  11887 825409 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ruby-block-autoloads.el ends here
