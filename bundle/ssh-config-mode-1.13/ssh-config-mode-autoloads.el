aa35d72edfa4c52e60ecbf33feaaba78c7e2b91e
Daniel Casimir Grigg <daniel.grigg@covata.com>
2013-09-06 14:39:39 +1000

    Add ssh-config-mode-1.13

---
+++ bundle/ssh-config-mode-1.13/ssh-config-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; ssh-config-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads (ssh-config-mode) "ssh-config-mode" "ssh-config-mode.el"
+;;;;;;  (21033 23591 0 0))
+;;; Generated autoloads from ssh-config-mode.el
+
+(autoload 'ssh-config-mode "ssh-config-mode" "\
+Major mode for fontifiying ssh config files.
+
+\\{ssh-config-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ssh-config-mode-pkg.el") (21033 23591
+;;;;;;  861096 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ssh-config-mode-autoloads.el ends here
