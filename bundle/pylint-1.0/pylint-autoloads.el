282a9054ac146eb8f1a767a9a45f31ee2a69c369
Daniel Casimir Grigg <daniel.grigg@covata.com>
2013-09-06 14:40:11 +1000

    Adding plugin pylint-1.0

---
+++ bundle/pylint-1.0/pylint-autoloads.el
@@ -0,0 +1,25 @@
+;;; pylint-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads (pylint) "pylint" "pylint.el" (21033 23595 0 0))
+;;; Generated autoloads from pylint.el
+
+(autoload 'pylint "pylint" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("pylint-pkg.el") (21033 23595 322448 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pylint-autoloads.el ends here
