7eb21ee0c97c8576144932103a439409377055d6
Daniel Casimir Grigg <daniel.grigg@covata.com>
2013-09-06 14:40:11 +1000

    Adding plugin pysmell-0.7.2

---
+++ bundle/pysmell-0.7.2/pysmell-autoloads.el
@@ -0,0 +1,16 @@
+;;; pysmell-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("pysmell-pkg.el" "pysmell.el") (21033
+;;;;;;  23594 317073 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pysmell-autoloads.el ends here
