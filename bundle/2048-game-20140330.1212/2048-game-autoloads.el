5793a55e9130dd4851b47a58f86dc2ae8f196b45
Daniel C Grigg <daniel.grigg@covata.com>
2014-05-01 12:56:02 +1000

    add scala stuff

---
+++ bundle/2048-game-20140330.1212/2048-game-autoloads.el
@@ -0,0 +1,15 @@
+;;; 2048-game-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("2048-game.el") (21345 46001 491918 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; 2048-game-autoloads.el ends here
---
+++ bundle/2048-game-20140330.1212/2048-game-pkg.el
@@ -0,0 +1,1 @@
+(define-package "2048-game" "20140330.1212" "play 2048 in Emacs" 'nil :url "https://bitbucket.org/zck/2048.el")
